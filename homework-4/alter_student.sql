c4698213380c294a11519183c47105c777d123fe
dem053 <142348050+dem053@users.noreply.github.com>
2023-11-06 22:43:48 +0400

    Update alter_student.sql
    5, 6, 7

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -18,9 +18,11 @@ ALTER TABLE student DROP COLUMN middle_name
 ALTER TABLE student RENAME COLUMN birthday TO birth_date
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+ALTER TABLE student ALTER COLUMN phone SET DATA TYPE  varchar(32)
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
-
+INSERT INTO student (first_name, last_name, birth_date, phone)  VALUES ('fname1', 'lname1', '01.01.2022', '7-111-111-11-11');
+INSERT INTO student (first_name, last_name, birth_date, phone)  VALUES ('fname2', 'lname2', '02.02.2022', '7-222-222-22-22');
+INSERT INTO student (first_name, last_name, birth_date, phone)  VALUES ('fname3', 'lname3', '03.03.2022', '7-333-333-33-33');
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+TRUNCATE TABLE student RESTART IDENTITY;
